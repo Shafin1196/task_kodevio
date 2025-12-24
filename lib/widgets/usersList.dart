30e6b9b15535df6ea3d62d7c700e807dbbefa70a
shafin1196 <shafin23105101196@diu.edu.bd>
2025-12-25 03:23:42 +0600

    refresh bug

--- lib/widgets/usersList.dart
+++ lib/widgets/usersList.dart
@@ -16,48 +16,47 @@ class _UsersListState extends ConsumerState<UsersList> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Consumer(
-        builder: (context, ref, child) {
-          final state = ref.watch(listProviderNotifierProvider);
-          if (state.errorMessage!='') {
-            return Center(child: Text(state.errorMessage));
-          }
-          if (state.users.isEmpty && state.search.isEmpty) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (state.filteredUsers.isEmpty) {
-            if (state.search.isNotEmpty) {
-              return const Center(child: Text('No users found for your search.'));
-            } else {
-              return const Center(child: Text('No users found.'));
-            }
-          }
-          return RefreshIndicator(
-            elevation: 10,
-            onRefresh: ()async{
-              await ref.read(listProviderNotifierProvider.notifier).fetchUsers();
-            },
-            child: ListView.builder(
-              itemCount: state.filteredUsers.length,
-              itemBuilder: (context, index) {
-                final user = state.filteredUsers[index];
-                return ListTile(
-                  title: Text(user.name ?? 'N/A'),
-                  subtitle: Text(user.email ?? 'N/A'),
-                  trailing: Text(user.company?.name??'N/A'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Userdetails(user: user),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          );
+      child: RefreshIndicator(
+        onRefresh: () async {
+          await ref.read(listProviderNotifierProvider.notifier).fetchUsers();
         },
+        child: Consumer(
+          builder: (context, ref, child) {
+            final state = ref.watch(listProviderNotifierProvider);
+            if (state.errorMessage!='') {
+              return Center(child: Text(state.errorMessage));
+            }
+            if (state.users.isEmpty && state.search.isEmpty) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (state.filteredUsers.isEmpty) {
+              if (state.search.isNotEmpty) {
+                return const Center(child: Text('No users found for your search.'));
+              } else {
+                return const Center(child: Text('No users found.'));
+              }
+            }
+            return ListView.builder(
+                itemCount: state.filteredUsers.length,
+                itemBuilder: (context, index) {
+                  final user = state.filteredUsers[index];
+                  return ListTile(
+                    title: Text(user.name ?? 'N/A'),
+                    subtitle: Text(user.email ?? 'N/A'),
+                    trailing: Text(user.company?.name??'N/A'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Userdetails(user: user),
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+          },
+        ),
       ),
     );
           
